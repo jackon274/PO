f3ab73c2f569e77462eab6c02b27d9d1cc250172
Jacek Konderak <287075@student.pwr.edu.pl>
2025-04-15 16:06:48 +0200

    Fix about window UI

--- Kalkulator_QT/aboutwindow.cpp
+++ Kalkulator_QT/aboutwindow.cpp
@@ -9,8 +9,22 @@ AboutWindow::AboutWindow(QWidget *parent)
     ui->setupUi(this);
     int fontID = QFontDatabase::addApplicationFont ( ":/Font/resources/SF-Pro-Rounded-Regular.otf" );
     QString family = QFontDatabase::applicationFontFamilies(fontID).at(0);
-    QFont appleFont(family);
-    ui->label_title->setFont(appleFont);
+    QFont appleFontBig(family);
+    appleFontBig.setPixelSize(25);
+    ui->label_title->setFont(appleFontBig);
+    ui->label_title->setAlignment(Qt::AlignCenter);
+
+    QFont appleFontMedium(family);
+    appleFontMedium.setPixelSize(18);
+    ui->label_name->setFont(appleFontMedium);
+    ui->label_index->setFont(appleFontMedium);
+
+    QFont appleFontSmall(family);
+    appleFontMedium.setPixelSize(11);
+    ui->label_copyright->setFont(appleFontSmall);
+    ui->label_compilation->setFont(appleFontSmall);
+
+
 }
 
 AboutWindow::~AboutWindow()
