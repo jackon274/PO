69f4941c39871f4ddfd7c377f1d309b89f86d79f
Jacek Konderak <287075@student.pwr.edu.pl>
2025-04-15 15:37:57 +0200

    Add about window

--- Kalkulator_QT/aboutwindow.cpp
+++ Kalkulator_QT/aboutwindow.cpp
@@ -1,11 +1,16 @@
 #include "aboutwindow.h"
 #include "ui_aboutwindow.h"
+#include <QFontDatabase>
 
 AboutWindow::AboutWindow(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::AboutWindow)
 {
     ui->setupUi(this);
+    int fontID = QFontDatabase::addApplicationFont ( ":/Font/resources/SF-Pro-Rounded-Regular.otf" );
+    QString family = QFontDatabase::applicationFontFamilies(fontID).at(0);
+    QFont appleFont(family);
+    ui->label_title->setFont(appleFont);
 }
 
 AboutWindow::~AboutWindow()
