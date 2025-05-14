fb8941e1b97af4d38ae6956db761d23455f612e5
Jacek <90107423+jackon274@users.noreply.github.com>
2025-05-14 20:08:47 +0200

    Fix crash for simulation QComboBox entry

--- GS/connectionwindow.cpp
+++ GS/connectionwindow.cpp
@@ -34,7 +34,7 @@ void ConnectionWindow::on_btn_refresh_clicked()
 {
     ui->btn_connect->setEnabled(true);
     ui->box_ports->clear();
-    ui->box_ports->addItem("Simulation");
+    ui->box_ports->addItem("Simulation", QVariant::fromValue(nullptr));
     /*serialPort.checkAvailableSerialPorts();
     std::vector <std::string> sp = serialPort.getAvailableSerialPorts();
     for(auto &a:sp) {
@@ -53,6 +53,9 @@ void ConnectionWindow::on_btn_connect_clicked()
     int baudRate = ui->box_baudrate->currentText().toInt();
     serialPortManager.setBaudRate(baudRate);
     SerialPort* selectedPort = ui->box_ports->currentData().value<SerialPort*>();
+    if(selectedPort == nullptr) {
+        return;
+    }
     std::cout << selectedPort->displayName << std::endl; //debug only
     serialPortManager.open(selectedPort);
 }
