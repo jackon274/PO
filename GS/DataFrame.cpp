139d735d751f47bfeaf10c22b7b4d218cefeef1c
Jacek <90107423+jackon274@users.noreply.github.com>
2025-05-19 15:31:15 +0200

    Create data frame class

---
+++ GS/DataFrame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jacek Konderak on 19/05/2025.
+//
+
+#include "DataFrame.h"
---
+++ GS/DataFrame.h
@@ -0,0 +1,16 @@
+//
+// Created by Jacek Konderak on 19/05/2025.
+//
+
+#ifndef DATAFRAME_H
+#define DATAFRAME_H
+
+
+
+class DataFrame {
+
+};
+
+
+
+#endif //DATAFRAME_H
