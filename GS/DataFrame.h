8ec682ce0a47394bea2d43172e623baa09bbfb8e
Jacek <90107423+jackon274@users.noreply.github.com>
2025-05-20 18:07:48 +0200

    Add data to DataFrame class

--- GS/DataFrame.h
+++ GS/DataFrame.h
@@ -4,10 +4,18 @@
 
 #ifndef DATAFRAME_H
 #define DATAFRAME_H
-
+#include <cstdint>
 
 
 class DataFrame {
+    int16_t temperature_outside;
+    int16_t temperature_inside;
+    int32_t longitude;
+    int32_t latitude;
+    uint32_t height;
+    uint16_t radiation_cpm;
+    uint8_t humidity_percent;
+    uint32_t checksum;
 
 };
 
