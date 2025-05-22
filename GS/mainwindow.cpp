f4becf55bca2a0850f9b8547315a10cdebe14781
Jacek <90107423+jackon274@users.noreply.github.com>
2025-05-22 22:25:11 +0200

    Add support for displaying map

--- GS/mainwindow.cpp
+++ GS/mainwindow.cpp
@@ -1,4 +1,7 @@
 #include "mainwindow.h"
+
+#include <QWebEngineView>
+#include "Map.h"
 #include "ui_mainwindow.h"
 #include "connectionwindow.h"
 
@@ -7,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    Map map(ui->map);
 }
 
 MainWindow::~MainWindow()
@@ -14,6 +18,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 SerialPortManager serialPortManager;
+
 void MainWindow::on_btn_connect_clicked() {
     ConnectionWindow window(serialPortManager);
     window.setModal(true);
