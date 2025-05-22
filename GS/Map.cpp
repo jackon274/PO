9f69d0ede3c552a1cea7cb454ba2dab8799c5b4f
Jacek <90107423+jackon274@users.noreply.github.com>
2025-05-22 19:00:14 +0200

    Add Map class

---
+++ GS/Map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jacek Konderak on 22/05/2025.
+//
+
+#include "Map.h"
---
+++ GS/Map.h
@@ -0,0 +1,16 @@
+//
+// Created by Jacek Konderak on 22/05/2025.
+//
+
+#ifndef MAP_H
+#define MAP_H
+
+
+
+class Map {
+
+};
+
+
+
+#endif //MAP_H
