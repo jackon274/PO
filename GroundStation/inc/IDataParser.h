0d0e713fa1dcc40fe93efecbb9d6c7725fcd0c08
Jacek <90107423+jackon274@users.noreply.github.com>
2025-06-06 15:32:47 +0200

    Add data parser interface

---
+++ GroundStation/inc/IDataParser.h
@@ -0,0 +1,20 @@
+//
+// Created by Jacek Konderak on 06/06/2025.
+//
+
+#ifndef IDATAPARSER_H
+#define IDATAPARSER_H
+
+
+
+class IDataParser {
+protected:
+
+public:
+    IDataParser();
+    virtual ~IDataParser() {}
+};
+
+
+
+#endif //IDATAPARSER_H
---
+++ GroundStation/src/IDataParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jacek Konderak on 06/06/2025.
+//
+
+#include "../inc/IDataParser.h"
