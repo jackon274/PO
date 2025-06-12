b31da5082ac790668122aea9af034c55ebb8ef1d
Jacek <90107423+jackon274@users.noreply.github.com>
2025-06-12 20:47:38 +0200

    Fix map on Linux

--- GroundStation/inc/Map.h
+++ GroundStation/inc/Map.h
@@ -17,7 +17,12 @@
 class Map {
     float longitude;
     float latitude;
+#ifdef __APPLE__
     QString filePath = QCoreApplication::applicationDirPath() + "/../Resources/map.html";
+#endif
+#ifdef __linux__
+    QString filePath = QCoreApplication::applicationDirPath() + "/Resources/map.html";
+#endif
     public:
     QWebEngineView* view;
     Map(QWebEngineView* ptrView);
